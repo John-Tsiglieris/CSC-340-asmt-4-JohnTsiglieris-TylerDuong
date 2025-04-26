a912e3df8537bf29ea68acab376ced74e3dff879
John-Tsiglieris <121003125+John-Tsiglieris@users.noreply.github.com>
2025-04-25 19:41:51 -0700

    Set up for later

--- Part1/seriesRecursive.cpp
+++ Part1/seriesRecursive.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <cstdlib> 
 
-//TODO: add your documentation here
-// TO DO: implement the recursive series function
-//      Hint: it should return a long long (integers)
+/**
+* COMPLETE THE DOCUMENTATION
+*/
 long long recurse(int n) {
-    
+    // Define base cases
+    // int result;
+    // If n > 2, result = recurse(n − 1) + 2 + recurse(n − 2) + 4 × J(n − 3)
+    // idk how to end the recursion when we get what we want
     return 0;
 }
+
+// EXAMPLE RECURSIVE FUNCTION TO COMPUTE FACTORIALS
+// Define a function to calculate factorial
+// recursively
+/*
+long long factorial(int n)
+{
+    // Base case - If n is 0 or 1, return 1
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    // Recursive case - Return n multiplied by
+    // factorial of (n-1)
+
+    return n * factorial(n - 1);
+}
+*/
 
 
 int main(int argc, char* argv[]) {
